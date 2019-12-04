f134f7ac452fc14226625900131c479562016128
Manasseh Mmadu <mmadumanasseh@gmail.com>
2019-12-04 16:23:27 +0100

    :sparkles: API integration with search-engine-parser library

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from search_engine_parser.core.engines import ENGINE_DICT
+from search_engine_parser.core.exceptions import NoResultsOrTrafficError
 
 app = FastAPI()
 
@@ -6,3 +8,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/search/")
+async def query(query: str, engine: str = "google", page: int = 1, limit: int = 10):
+    try:
+        _engine = ENGINE_DICT[engine]()
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Specified Engine Does not Exist")
+    results = await _engine.async_search(query, page)
+    return results
+
